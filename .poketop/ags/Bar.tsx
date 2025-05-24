f96d7003cc56cd8efa96dcf676a6f3c2965d35e9
Knee <lukeedo@icloud.com>
2025-05-24 14:45:17 -0400

    the thing!

--- .poketop/ags/Bar.tsx
+++ .poketop/ags/Bar.tsx
@@ -4,14 +4,14 @@ import { Variable } from "astal"
 const time = Variable("").poll(1000, "date")
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+    const { BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
 
     return <window
         visible
         cssClasses={["Bar"]}
         gdkmonitor={gdkmonitor}
         exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={TOP | LEFT | RIGHT}
+        anchor={BOTTOM | LEFT | RIGHT}
         application={App}>
         <centerbox cssName="centerbox">
             <button
